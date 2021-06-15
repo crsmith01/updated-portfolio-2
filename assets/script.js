4d1cf998890f416382fb4c946c5888c0b96d6110
Catherine Smith <catsmith24601@gmail.com>
2021-06-15 17:38:57 -0400

    tooltips are working, reorganized assets into folder, removed htmls with experimenting

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// to enable Bootstrap tooltips in contact section
+// delay - 200 milliseconds; animation - CSS fade effect takes place; trigger - hover, ass opposed to flick or focus
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip({delay: 200, animation: true, trigger: "hover"});
+});
